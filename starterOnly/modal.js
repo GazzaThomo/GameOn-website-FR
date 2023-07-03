415ab9048462c62c91435f4b986b2ec0cde37ee2
Gareth Thomas <thomasgareth3@gmail.com>
2023-07-03 23:39:09 +0200

    feat:modal close

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,24 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//This part of the code can be refactored later
+//close modal event
+modalbg.addEventListener("click", closeModal);
+modalCloseBtn.addEventListener("click", closeModal);
+document.addEventListener("keydown", closeModalEscape);
 
+//close modal on X click or if click is outside the content field
+function closeModal(e) {
+  console.log(e.target);
+  if (e.target === modalbg || e.target === modalCloseBtn) {
+    modalbg.style.display = "none";
+  }
+}
+
+//for accessibility, if escape is pressed whilst modal is open.
+function closeModalEscape(e) {
+  const style = window.getComputedStyle(modalbg);
+  if (style.display === "block" && e.key === "Escape") {
+    modalbg.style.display = "none";
+  }
+}
